58e36e85307d72c7d5a8f924197860c350ab9e7f
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-09 09:46:12 -0700

    Created the model

---
+++ TravelDiary/Model/Entry.swift
@@ -0,0 +1,23 @@
+//
+//  Entry.swift
+//  TravelDiary
+//
+//  Created by Jake Gloschat on 2/9/23.
+//
+
+import Foundation
+
+class Entry {
+    let title: String
+    let address: String
+    let entryDate: Date
+    let body: String
+    
+    init(title: String, address: String, entryDate: Date, body: String)
+    self.title = title
+    self.address = address
+    self.entryDate = entryDate
+    self.body = body
+    
+    }
+} // End of class
