817e4d9c65d6ce439a47790847719ad26ee218fa
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-09 12:01:42 -0700

    Tableview nearly completed

--- TravelDiary/View Controllers/Custom Views/EntryTableViewCell.swift
+++ TravelDiary/View Controllers/Custom Views/EntryTableViewCell.swift
@@ -10,10 +10,14 @@ import UIKit
 class EntryTableViewCell: UITableViewCell {
 
 // MARK: - Outlets
-    @IBOutlet weak var entryNameLabel: UILabel!
+    @IBOutlet weak var entryTitleLabel: UILabel!
     @IBOutlet weak var entryAddressLabel: UILabel!
     @IBOutlet weak var entryDateLabel: UILabel!
     
-
+    func updateViews(with entryToDisplay: Entry) {
+        entryTitleLabel.text = entryToDisplay.title
+        entryAddressLabel.text = entryToDisplay.address
+        entryDateLabel.text = entryToDisplay.entryDate.stringValue()
+    }
     
 }
