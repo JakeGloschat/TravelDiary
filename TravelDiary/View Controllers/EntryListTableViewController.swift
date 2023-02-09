6d5dc96841f0f14ce230a9f6080a7f3a56f3642a
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-09 12:30:10 -0700

    Can now create entries

--- TravelDiary/View Controllers/EntryListTableViewController.swift
+++ TravelDiary/View Controllers/EntryListTableViewController.swift
@@ -8,32 +8,34 @@
 import UIKit
 
 class EntryListTableViewController: UITableViewController {
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return EntryController.sharedInstance.entries.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath) as? EntryTableViewCell else {return UITableViewCell()}
-
+        
         // Configure the cell...
         let entryForTheCellToUpdateWith = EntryController.sharedInstance.entries[indexPath.row]
         cell.updateViews(with: entryForTheCellToUpdateWith)
         return cell
     }
-  
-
+    
+    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -42,19 +44,19 @@ class EntryListTableViewController: UITableViewController {
             let entryToBeDeleted = EntryController.sharedInstance.entries[indexPath.row]
             EntryController.sharedInstance.delete(entryToDelete: entryToBeDeleted)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        }    
+        }
     }
     
-  
-
+    
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     
-
+    
 }
